e1c1553ba4a68be4e320dd9f0ed937367689c953
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-07-15 00:02:10 -0400

    Day 21

--- main.py
+++ main.py
@@ -1,48 +1,24 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-import time
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('My Snake Game')
-screen.tracer(0)
-
-snake = Snake()
-food = Food()
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    def breathe(self):
+        print("Inhale, exhale.")
 
 
-# create turtles
-# segment_1 = Turtle(shape='square')
-# segment_1.color("white")
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
 
-# segment_2 = Turtle(shape='square')
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
 
-# segment_3 = Turtle(shape='square')
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
-
-# create turtles using a for loop and tuples
+    def swim(self):
+        print("moving in water.")
 
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-
-    snake.move()
-
-    # Detect collision with food.
-    if snake.head.distance(food) < 15:
-        food.refresh()
-
-screen.exitonclick()
+nemo = Fish()
+# nemo.swim()
+nemo.breathe()
+# print(nemo.num_eyes)
