5a057d6a1ff4e6aebaf20553fe99c6b960de25cd
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-07-16 20:40:38 -0400

    Day 24

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-class Animal:
-    def __init__(self):
-        self.num_eyes = 2
-
-    def breathe(self):
-        print("Inhale, exhale.")
+with open('new_file.text', mode='w') as file:
+    file.write("New text.")
 
 
-class Fish(Animal):
-    def __init__(self):
-        super().__init__()
-
-    def breathe(self):
-        super().breathe()
-        print("doing this underwater.")
-
-    def swim(self):
-        print("moving in water.")
-
-
-nemo = Fish()
-# nemo.swim()
-nemo.breathe()
-# print(nemo.num_eyes)
+with open('text.text') as file:
+    contents = file.read()
+    print(contents)
