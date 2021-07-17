3ce63140e799645814d7bfd03f00dd6b5abf0e44
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-07-17 07:57:41 -0400

    Day 25

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-with open('new_file.text', mode='w') as file:
-    file.write("New text.")
+# with open('weather_data.csv') as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+# import os
+
+# cwd = os.getcwd()  # get the cuent working directory (cwd)
+# files = os.listdir(cwd)  # get all the files in that directory
+# print("Files in %r: % s" % (cwd, files))
 
 
-with open('text.text') as file:
-    contents = file.read()
-    print(contents)
+# with open('Day 25/weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+data = pandas.read_csv('Day 25/weather_data.csv')
+print(type(data))
