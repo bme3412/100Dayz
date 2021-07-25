77630e4c0ac86cf431ba2f6c1a27fc6f91cbfae7
bme3412 <54747855+bme3412@users.noreply.github.com>
2021-07-25 08:25:36 -0400

    Day 33

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-# import smtplib
+import requests
 
-# my_email = "erhardbr@gmail.com"
-# password = "python1429$$"
-
-# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs=my_email,
-#                         msg="Subject:Hello\n\nThis is the body of my email")
-
-import datetime as dt
-
-# get current data and time
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-
-
-date_of_birth = dt.datetime(year=1987, month=10, day=26, hour=7, minute=15)
-print(date_of_birth)
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
